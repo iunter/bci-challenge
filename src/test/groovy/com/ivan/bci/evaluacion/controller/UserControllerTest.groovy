1f98ef4f43803de01b60c279cd899c39c06bf690
iunter <ivounter@gmail.com>
2024-02-28 13:19:57 -0300

    Mejorados tests

--- src/test/groovy/com/ivan/bci/evaluacion/controller/UserControllerTest.groovy
+++ src/test/groovy/com/ivan/bci/evaluacion/controller/UserControllerTest.groovy
@@ -1,6 +1,8 @@
 import com.ivan.bci.evaluacion.controller.UserController
+import com.ivan.bci.evaluacion.model.User
 import com.ivan.bci.evaluacion.model.UserRequest
 import com.ivan.bci.evaluacion.service.UserService
+import org.springframework.http.HttpStatus
 import spock.lang.Specification
 
 class UserControllerTest extends Specification
@@ -8,7 +10,32 @@ class UserControllerTest extends Specification
 
     def "addUserTest"() {
         given:
-        UserService service = Mock(UserService)
+        def service = Stub(UserService)
+
+        def user = new User()
+
+        def userController = new UserController(service)
+
+        def userRequest = UserRequest.builder()
+                .email("email@email.com")
+                .name("name")
+                .password("password")
+                .phones(Collections.emptyList())
+                .build()
+
+        service.addUser(userRequest) >> user
+
+        when:
+        def response = userController.newUser(userRequest)
+
+        then:
+        response.getBody() == user
+        response.getStatusCode() == HttpStatus.OK
+    }
+
+    def "addUserErrorTest"() {
+        given:
+        UserService service = Stub(UserService)
         UserController userController = new UserController(service)
         UserRequest userRequest = UserRequest.builder()
                 .email("email@email.com")
@@ -17,10 +44,13 @@ class UserControllerTest extends Specification
                 .phones(Collections.emptyList())
                 .build()
 
+        service.addUser(_) >> {throw new Exception("Test")}
+
         when:
-        userController.newUser(userRequest)
+        def response = userController.newUser(userRequest)
 
         then:
-        1 * service.addUser(userRequest)
+        response.getBody() == "{\"mensaje\": \"Test\""
+        response.getStatusCode() == HttpStatus.BAD_REQUEST
     }
 }
